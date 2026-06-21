bef5648dcd9688ae6e953346b29d021cfc3e38ab
Johannes <johannes.programming@gmail.com>
2026-06-22 01:40:21 +0200

    Version 0.2.1

--- src/superparsing/__init__.py
+++ src/superparsing/__init__.py
@@ -3,4 +3,4 @@ from superparsing.core.SuperFlag import SuperFlag
 from superparsing.core.SuperParseError import SuperParseError
 from superparsing.core.SuperParser import SuperParser
 
-__all__ = ["SuperParseError", "SuperFlag", "SubCommand", "SuperParser"]
+__all__ = ["SubCommand", "SuperFlag", "SuperParseError", "SuperParser"]
